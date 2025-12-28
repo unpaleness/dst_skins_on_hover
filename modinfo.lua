f4de03f4c56bad873e0f5cbea781575cf8994cb3
Egor Morunov <generalissimusunpaleness@gmail.com>
2025-12-28 12:42:35 +0100

    start v0.0.1

---
+++ modinfo.lua
@@ -0,0 +1,28 @@
+name = "Show skins amount"
+description = [[
+Version: 0.0.1
+Features:
+- shows amount of available skins on hover
+]]
+author = "unpaleness"
+version = "0.0.1"
+
+forumthread = ""
+
+api_version = 10
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+hamlet_compatible = false
+dst_compatible = true
+
+all_clients_require_mod = false
+client_only_mod = true
+
+priority = 0
+
+icon_atlas = "images/icon.xml"
+icon = "icon.tex"
+
+configuration_options = {}
